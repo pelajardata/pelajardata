5dec9e3dfb6d097613008c56dfa1a1e6a524b40d
imandreans <andrean.sihombing83@gmail.com>
2025-05-06 19:12:29 +0800

    Tidy up the UI

--- __test__/components/intro.tsx
+++ __test__/components/intro.tsx
@@ -3,7 +3,7 @@ return <div id="intro">
       <section>
         <h1 id='persuasive-text'>CONNECT AND GROW, TOGETHER <br/>
         WITH PELAJAR DATA</h1>
-        <a href="">Join Us</a>
+        <a href="https://discord.gg/EGCKArUr">Join Us</a>
         </section>
         <svg width="854" height="480" viewBox="0 0 854 480" fill="none" xmlns="http://www.w3.org/2000/svg">
 <mask id="mask0_327_645" maskUnits="userSpaceOnUse" x="0" y="0" width="854" height="480">

2c5c0417b5b2ccb4e3c490cd929c66e04a2bfe1e
pelajardata <pelajardataid@gmail.com>
2025-05-10 00:48:13 +0700

    server changes

--- __test__/components/footer.tsx
+++ __test__/components/footer.tsx
@@ -1,23 +1,24 @@
 import XIcon from '@mui/icons-material/X';
 import EmailIcon from '@mui/icons-material/Email';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import React from 'react';
 const logo = '/Logo.svg'
 
 function footer(){
     return <footer>
-    <div>
-      <img id="logo" src={logo} alt="logo" /> <h1>PELAJAR DATA</h1>
+    <div className="footer-branding">
+      <img id="logo" src={logo} alt="logo" /> 
+      <h1>PELAJAR DATA</h1>
     </div>
     <div id="contacts">
-      <a href="https://x.com/pelajardata">
-    <XIcon />
+      <a href="https://x.com/pelajardata" aria-label="Twitter">
+        <XIcon />
       </a>
-      <a href="mailto:pelajardataid@gmail.com">
-    <EmailIcon />
+      <a href="mailto:pelajardataid@gmail.com" aria-label="Email">
+        <EmailIcon />
       </a>
-
-      <a href="https://www.instagram.com/pelajardata/">
-    <InstagramIcon />
+      <a href="https://www.instagram.com/pelajardata/" aria-label="Instagram">
+        <InstagramIcon />
       </a>
     </div>
       </footer>
